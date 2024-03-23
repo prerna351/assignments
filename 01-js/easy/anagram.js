a5ad49cf0a54d4b297884c8986962dbd81718920
prerna351 <91184749+prerna351@users.noreply.github.com>
2024-03-23 15:44:24 +0530

    solved anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,11 @@
 */
 
 function isAnagram(str1, str2) {
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+    const sortedStr2 = cleanStr2.split('').sort().join('');
 
+    // Compare the sorted strings
+    return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
