0f744874f40a93aeb59b16e66fcdb6474b504d95
prerna351 <91184749+prerna351@users.noreply.github.com>
2024-03-25 00:10:23 +0530

    Update 03-errorcount.js

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,10 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use(function(err,req,res,next){
+   res.status(404).json({\
+     msg: "Page not found"
+   }) 
+})
+
 module.exports = app;
